4f0a2aec4cc872e0c53ef27aa00954150e882280
Thomas Sharkey <tbsharkey@gmail.com>
2018-08-26 05:59:21 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ function  logWhisper(string) {
 
 function sayHiToGrandma(string) {
   if (string === string.toUpperCase()){
-    console.log("I can't hear you!")
+    return 'I can\'t hear you!'
     }
   else if (string === string.toUpperCase()) {
     console.log("YES INDEED!")
